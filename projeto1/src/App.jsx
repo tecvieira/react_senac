3d4ebff0f460eb5a94c8fee0c6fbd26e82a78601
Robson Vieira <tecvander.vieira@gmail.com>
2026-01-17 18:39:08 -0300

    corrigiu algumas configurações antigas

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './Header.jsx'
 import Body from './Body.jsx'
 import Footer from './Footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <Header></Header>
