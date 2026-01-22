abea8b39e24e249a27f7a74d8b319aa7d17b067d
Robson Vieira <tecvander.vieira@gmail.com>
2026-01-22 07:16:57 -0300

    colocando fusca

--- projeto6-carros/src/Body.jsx
+++ projeto6-carros/src/Body.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import Fuscared from './assets/img/fusca_red.png';
 
 export default function Body() {
     
     return (
         <body>
-            <div class="container text-center">
-                <div class="row">
+            <div className="container text-center my-5">
+                <div class="row align-items-center">
                     
-                    <div class="col">
-                        <h2>Veículos Populares</h2>
+                    <div className="col">
+                        <h2>Meus Carros</h2>
+                        <img src={Fuscared} className="img-fluid" alt="react imagem" />
                         
                     </div>
                     <div class="col">
                         <h2>Sua História começa aqui.</h2>
+                        <div class="card">
+                            <div className="card-body w-100">
+                                O primeiro carro que tive foi um fusca 1969 vermelho, que foi comprado com ajuda da minha mãe. Tive esse carro por muitos anos, até que conseguisse juntar dinheiro para comprar o meu segundo carro, um chevete 1979 branco.
+                            </div>
+                        </div>
                         
                     </div>
                     
